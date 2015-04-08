579285ecdddca7606238a6d0c80197748d2d506b
Alexander <sharrikk@gmail.com>
2015-04-09 02:03:30 +0300

    init

---
+++ HW_CoreDataFileSystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW_CoreDataFileSystem
+//
+//  Created by Alexander on 09.04.15.
+//  Copyright (c) 2015 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
